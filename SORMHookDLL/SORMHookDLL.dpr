4778be14fe6834a611483c0d30bf690754377215
Duncan MacKirik <duncanmackirik@gmail.com>
2024-12-09 15:31:54 +0300

    Added code for the Shadow Ops: Red Mercury patch

---
+++ SORMHookDLL/SORMHookDLL.dpr
@@ -0,0 +1,17 @@
+library SORMHookDLL;
+
+uses
+  HookDLL in '..\HookDLL.pas' {$R *.res},
+  Winapi.DirectDraw;
+
+{$R *.res}
+
+exports
+     DirectDrawCreate;
+
+begin
+     Sequence := [$53, $56, $8B, $F1, $8B, $08, $33, $DB,
+          $3B, $CB, $57, $89, $65, $F0];
+     CodeOffset := $2A;
+     InitDLL;
+end.
